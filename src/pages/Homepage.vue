ced6420e7884a710e94b96a93f2937808e349e09
Dami Ajayi <ajayidamey@gmail.com>
2021-02-20 23:55:12 +0100

    setup app and skeleton template

---
+++ src/pages/Homepage.vue
@@ -0,0 +1,63 @@
+<template>
+  <main>
+      <div class="section-container">
+        <p class="logo__body">
+        <img
+            src="http://placehold.it/200x30"
+            alt="RenewableTek Africa Logo"
+            class="logo"
+        />
+        </p>
+        <header class="section__header">
+        <h2>
+            RenewableTek Africa
+        </h2>
+        </header>
+        <section class="hero__body">
+            <div class="hero-body__text">
+                <p>
+                RenewableTek Africa is the premier renewable energy consulting firm
+                providing a range of services including; engineering, procurement,
+                construction, maintenance, training and development of renewable
+                energy assets across sub-saharan Africa.
+                </p>
+            </div>
+            <div>
+                <img src="http://placehold.it/600x400" alt="Engineering Projects" />
+            </div>
+        </section>
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "homepage",
+};
+</script>
+
+<style scoped>
+.logo__body {
+  min-height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.hero-body__text {
+    max-width: 45rem;
+}
+.hero-body__text p {
+    font-size: 2rem;
+}
+@media screen and (min-width: 768px) {
+    .hero__body {
+        display: flex;
+        justify-content: space-between;
+    }
+    .hero-body__text p {
+        font-size: 2rem;
+        margin-top: 10rem;
+    }
+}
+</style>
---
+++ src/pages/WayOfBusiness.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+      <div class="section-container">
+        <header class="section__header">
+            <h2>
+                These four principles govern the way we do business:
+            </h2>
+        </header>
+        <section class="business__principles">
+            <div class="business-principles__textbox"></div>
+            <div class="business-principles__textbox"></div>
+            <div class="business-principles__textbox"></div>
+            <div class="business-principles__textbox"></div>
+        </section>
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "way-of-business",
+};
+</script>
+
+<style scoped>
+    .business__principles {
+        width: 90%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+    }
+    .business-principles__textbox {
+        width: 20%;
+        border-radius: 10px;
+        height: 20rem;
+        background: var(--accentTwo);
+    }
+</style>
